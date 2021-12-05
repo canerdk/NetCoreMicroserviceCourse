4888b256d9970e61748cded1e9c63d47f603b38a
caner <canerdk1995@hotmail.com>
2021-12-06 00:13:31 +0300

    installed auto mapper and configured

---
+++ NetCoreMicroservices/Services/Catalog/Catalog.API/Mapping/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Catalog.API.Dtos;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Mapping
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
