568fa708d41da3f6463f525608da4576c2c3c2cb
Caner <caner.demirkaya@secilstore.com>
2021-12-15 11:07:19 +0300

    configured identity server

---
+++ NetCoreMicroservices/IdentityServer/IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dtos;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.Username,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+    }
+}
